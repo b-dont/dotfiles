0033c0997024633fce4c706f7674bbabc4d2f6af
Brandon Phillips <btp@brandont.dev>
2023-01-13 13:00:18 -0700

    Added verbose output to nvim addons update script

--- scripts/update_nvim_addons.sh
+++ scripts/update_nvim_addons.sh
@@ -6,6 +6,17 @@ start_dir=$(pwd)
 for d in $(find ~/.local/share/nvim/site/pack/local/start -maxdepth 1 -type d)
 do
 	cd "$d" || exit
+
+	# Grab the current directory without the full path
+	# correct if the case is PWD=/
+	current_dir=${PWD##*/}
+	current_dir=${current_dir:-/}
+
+	# Print the current_dir to stdout
+	printf '%s ' "Updating: $current_dir"
+
+	# Pull from the remote repo to update
+	# .. then move back to the parent dir
 	git pull
 	cd ..
 done
