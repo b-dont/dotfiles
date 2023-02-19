544e54b6a902205ea6687af925c16151e3c7cc0e
Brandon Phillips <btp@brandont.dev>
2023-02-19 07:43:59 -0700

    Some refactors in the install script

--- scripts/install_dots.sh
+++ scripts/install_dots.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 start_dir=$(pwd)
-nvimdir=~/.config/nvim/
-alacrittydir=~/.config/alacritty/
-helix_dir=~/.config/helix/
-dots_dir=~/dotfiles/dots/
+nvimdir=~/.config/nvim
+alacrittydir=~/.config/alacritty
+helix_dir=~/.config/helix
+dots_dir=~/dotfiles/dots
 omz_dir=~/.oh-my-zsh/custom/plugins
 
 if ! [[ -d "$helix_dir" ]]
@@ -33,15 +33,17 @@ fi
 
 echo "Creating sym-link of init.vim at $nvimdir"
 ln -s ~/dotfiles/configs/init.vim $nvimdir/init.vim
+
 echo "Creating sym-link of alacritty.yml at $alacrittydir"
 ln -s ~/dotfiles/configs/alacritty.yml $alacrittydir/alacritty.yml
+
 echo "Creating sym-link of helix config directory at $helix_dir"
 ln -s ~/dotfiles/configs/helix $helix_dir
 
 shopt -s dotglob
 for f in "$dots_dir"*; do
 	echo "Creating sym-link of '$f' at '$HOME'"
-	ln -s "$HOME/dotfiles/dots/$f" "$HOME/.$f"
+	ln -s "$HOME/$dots_dir/$f" "$HOME/.$f"
 done
 
 echo "Installing oh-my-zsh plugins"
