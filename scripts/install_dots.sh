f857af72e5d46f54150474c88ce4850eecdca646
Brandon Phillips <btp@brandont.dev>
2023-03-17 10:12:28 -0600

    Refactored install script

--- scripts/install_dots.sh
+++ scripts/install_dots.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
 start_dir=$(pwd)
+
 nvimdir=~/.config/nvim
 alacrittydir=~/.config/alacritty
 helix_dir=~/.config/helix
 dots_dir=~/dotfiles/dots
+configs_dir=~/dotfiles/configs
 omz_dir=~/.oh-my-zsh/custom/plugins
 
-if ! [[ -d "$helix_dir" ]]
-then
-	echo "Directory '~/.config/helix' not found. Creating directory."
-	mkdir -p $helix_dir
-fi
+dirs=(omz_dir, helix_dir, alacrittydir, nvimdir)
+configs=("init.vim", "alacritty.yml", "$helix_dir")
 
-if ! [[ -d "$alacrittydir" ]]
-then
-	echo "Directory '~/.config/alacritty' not found. Creating directory."
-	mkdir -p $alacrittydir
-fi
-
-if ! [[ -d "$nvimdir" ]]
-then
-	echo "Directory '~/.config/nvim' not found. Creating directory."
-	mkdir -p $nvimdir
-fi
-
-if ! [[ -d "$omz_dir" ]]
-then
-	echo "Directory '~/.oh-my-zsh/custom/plugins' not found. Creating directory."
-	mkdir -p $omz_dir
-fi
-
-echo "Creating sym-link of init.vim at $nvimdir"
-ln -s ~/dotfiles/configs/init.vim $nvimdir/init.vim
-
-echo "Creating sym-link of alacritty.yml at $alacrittydir"
-ln -s ~/dotfiles/configs/alacritty.yml $alacrittydir/alacritty.yml
-
-echo "Creating sym-link of helix config directory at $helix_dir"
-ln -s ~/dotfiles/configs/helix $helix_dir
+for dir in $dirs; do
+	if ! [[ -d "$dir" ]]; then
+		echo "Directory $dir not found. Creating $dir."
+		mkdir -p $dir
+		if [ $dir = $nvimdir ]; then
+			echo "Creating sym-link of init.vim at $dir"
+			ln -s $configs_dir/init.vim $dir/init.vim
+		elif [ $dir = $alacrittydir ]; then
+			echo "Creating sym-link of alacritty.yml at $dir"
+			ln -s $configs_dir/alacritty.yml $dir/alacritty.yml
+		elif [ $dir = $helix_dir ]; then
+			echo "Creating sym-link of helix config directory at $dir"
+			ln -s $configs_dir/helix $dir
+	fi
+done
 
 shopt -s dotglob
 for f in "$dots_dir"*; do
