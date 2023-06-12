e40ddb5b0dad1c032a0fd7f5d40e15dfc44f6668
Brandon Phillips <btp@brandont.dev>
2023-06-12 12:30:15 -0600

    Added line to tmux.conf

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euf
+
+cd $HOME/.dotfiles/dots/$1 || exit
+
+pushd $HOME/.dotfiles/dots/$1
+packages=(*)
+popd
+
+if ! [ -z "$1" ] && [ -z "$2" ]
+then
+  for package in "$packages"; 
+  do
+    stow -t "$HOME" -S "$package"
+  done
+fi
+
+if ! [ -z "$2" ]
+then
+  stow -t "$HOME" -S "$2"
+fi
